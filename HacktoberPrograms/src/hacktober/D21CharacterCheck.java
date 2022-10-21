ae0a33d9d67ec694573be6b2a11b00b8982a0934
home <home@172.17.54.220>
2022-10-21 11:06:08 +0530

    commit

---
+++ HacktoberPrograms/src/hacktober/D21CharacterCheck.java
@@ -0,0 +1,31 @@
+package hacktober;
+
+import java.util.Scanner;
+
+public class D21CharacterCheck {
+
+	public static void main(String[] args) {
+		D21CharacterCheck task = new D21CharacterCheck();
+		task.display();
+	}
+
+	void display() {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a word : ");
+		String word = scan.nextLine();
+		System.out.print("Enter a character : ");
+		char letter = scan.nextLine().charAt(0);
+		scan.close();
+		
+		System.out.println(checkConsecutiveCharacters(word,0,letter));
+	}
+	
+	boolean checkConsecutiveCharacters(String word,int index,char letter) {
+		if(index==word.length())
+			return false;
+		if(word.charAt(index)==letter)
+			return true;
+		return checkConsecutiveCharacters(word,index+1,letter);	
+	}
+
+}
